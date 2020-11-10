352e1cc865fd4ba76c8dce68f26b58cdab7684e7
Juan Rodrigo Vicario <juanrodrigovicario@gmail.com>
2020-11-10 08:35:50 -0300

    subiendo a git

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
-document.addEventListener('DOMContentLoaded', () =>{
-    const elementosCarousel =document.querySelectorAll('carousel');
-    M.Carousel.init(elementosCarousel, {
-        duration:150
+let comentarios = ["Excelente Servicio", "Muy buen Servicio", "mal servicio","probando serivicio"],
+cont = 0;
+
+function carrousel(contenedor) {
+    contenedor.addEventListener('click', e => {
+        let atras = contenedor.querySelector('.atras'),
+            adelante = contenedor.querySelector('.adelante'),
+            q = contenedor.querySelector('.q'),
+            tgt = e.target;
+
+            if(tgt==atras){
+                if (cont>0){
+                    q.src =comentarios[cont -1];
+                    cont--;
+                }else {
+                    q.src = comentarios [comentarios.length -1];
+                    cont = comentarios.length -1;
+                }
+            }else if(tgt==adelante){
+                if (cont < comentarios.length -1){
+                    q.src =comentarios[cont +1];
+                    cont++;
+                }else {
+                    q.src = comentarios [0];
+                    cont =0;
+                }
+            }
     });
+} 
+
+document.addEventListener("DOMContentLoaded", () => {
+    let contenedor = document.querySelector('.contenedor');
+    carrousel(contenedor);   
 });

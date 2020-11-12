624afa9abd36b21d39c8a022ca6457fe936742c0
Juan Rodrigo Vicario <juanrodrigovicario@gmail.com>
2020-11-12 18:10:26 -0300

    finalizada seccion preguntas frecuentes

---
+++ dist/preguntasFrecuentes.dev.js
@@ -0,0 +1,156 @@
+"use strict";
+
+var pregunta1 = document.getElementById('pregunta1');
+pregunta1.addEventListener('click', function () {
+  // console.log('hola')
+  var parrafos = document.getElementById('info-desplegable1');
+
+  if (parrafos.style.display == "none") {
+    parrafos.style.display = "block";
+  } else {
+    parrafos.style.display = "none";
+  }
+});
+var pregunta2 = document.getElementById('pregunta2');
+pregunta2.addEventListener('click', function () {
+  // console.log('hola')
+  var parrafos = document.getElementById('info-desplegable2');
+
+  if (parrafos.style.display == "none") {
+    parrafos.style.display = "block";
+  } else {
+    parrafos.style.display = "none";
+  }
+});
+var pregunta3 = document.getElementById('pregunta3');
+pregunta3.addEventListener('click', function () {
+  // console.log('hola')
+  var parrafos = document.getElementById('info-desplegable3');
+
+  if (parrafos.style.display == "none") {
+    parrafos.style.display = "block";
+  } else {
+    parrafos.style.display = "none";
+  }
+});
+var pregunta4 = document.getElementById('pregunta4');
+pregunta4.addEventListener('click', function () {
+  // console.log('hola')
+  var parrafos = document.getElementById('info-desplegable4');
+
+  if (parrafos.style.display == "none") {
+    parrafos.style.display = "block";
+  } else {
+    parrafos.style.display = "none";
+  }
+});
+var pregunta5 = document.getElementById('pregunta5');
+pregunta5.addEventListener('click', function () {
+  // console.log('hola')
+  var parrafos = document.getElementById('info-desplegable5');
+
+  if (parrafos.style.display == "none") {
+    parrafos.style.display = "block";
+  } else {
+    parrafos.style.display = "none";
+  }
+});
+var pregunta6 = document.getElementById('pregunta6');
+pregunta6.addEventListener('click', function () {
+  // console.log('hola')
+  var parrafos = document.getElementById('info-desplegable6');
+
+  if (parrafos.style.display == "none") {
+    parrafos.style.display = "block";
+  } else {
+    parrafos.style.display = "none";
+  }
+});
+var pregunta7 = document.getElementById('pregunta7');
+pregunta7.addEventListener('click', function () {
+  // console.log('hola')
+  var parrafos = document.getElementById('info-desplegable7');
+
+  if (parrafos.style.display == "none") {
+    parrafos.style.display = "block";
+  } else {
+    parrafos.style.display = "none";
+  }
+});
+var pregunta8 = document.getElementById('pregunta8');
+pregunta8.addEventListener('click', function () {
+  // console.log('hola')
+  var parrafos = document.getElementById('info-desplegable8');
+
+  if (parrafos.style.display == "none") {
+    parrafos.style.display = "block";
+  } else {
+    parrafos.style.display = "none";
+  }
+});
+var pregunta9 = document.getElementById('pregunta9');
+pregunta9.addEventListener('click', function () {
+  // console.log('hola')
+  var parrafos = document.getElementById('info-desplegable9');
+
+  if (parrafos.style.display == "none") {
+    parrafos.style.display = "block";
+  } else {
+    parrafos.style.display = "none";
+  }
+});
+var pregunta10 = document.getElementById('pregunta10');
+pregunta10.addEventListener('click', function () {
+  // console.log('hola')
+  var parrafos = document.getElementById('info-desplegable10');
+
+  if (parrafos.style.display == "none") {
+    parrafos.style.display = "block";
+  } else {
+    parrafos.style.display = "none";
+  }
+});
+var pregunt11 = document.getElementById('pregunta11');
+pregunta11.addEventListener('click', function () {
+  // console.log('hola')
+  var parrafos = document.getElementById('info-desplegable11');
+
+  if (parrafos.style.display == "none") {
+    parrafos.style.display = "block";
+  } else {
+    parrafos.style.display = "none";
+  }
+});
+var pregunta12 = document.getElementById('pregunta12');
+pregunta12.addEventListener('click', function () {
+  // console.log('hola')
+  var parrafos = document.getElementById('info-desplegable12');
+
+  if (parrafos.style.display == "none") {
+    parrafos.style.display = "block";
+  } else {
+    parrafos.style.display = "none";
+  }
+});
+var pregunta13 = document.getElementById('pregunta13');
+pregunta13.addEventListener('click', function () {
+  // console.log('hola')
+  var parrafos = document.getElementById('info-desplegable13');
+
+  if (parrafos.style.display == "none") {
+    parrafos.style.display = "block";
+  } else {
+    parrafos.style.display = "none";
+  }
+});
+var pregunta14 = document.getElementById('pregunta14');
+pregunta14.addEventListener('click', function () {
+  // console.log('hola')
+  var parrafos = document.getElementById('info-desplegable14');
+
+  if (parrafos.style.display == "none") {
+    parrafos.style.display = "block";
+  } else {
+    parrafos.style.display = "none";
+  }
+});
